ef62e1934b523b366cc5fa0aaf20bbadd9b66a85
James Blair <james@blair.nz>
2026-07-09 09:23:06 +1200

    Make GameTopBar be fixed to the top of the page

--- components/GameTopBar.tsx
+++ components/GameTopBar.tsx
@@ -3,11 +3,13 @@ import { SidebarTrigger } from "./ui/sidebar";
 
 export function GameTopBar() {
   return (
-    <div className="border-b p-2">
-      <SidebarTrigger>
-        <IconLayoutSidebar />
-        <span className="sr-only">Toggle Sidebar</span>
-      </SidebarTrigger>
+    <div className="h-[45.5px]">
+      <div className="bg-background fixed w-full border-b p-2">
+        <SidebarTrigger>
+          <IconLayoutSidebar />
+          <span className="sr-only">Toggle Sidebar</span>
+        </SidebarTrigger>
+      </div>
     </div>
   );
 }
