99c394c605a3a9d75bfc466bf0db2ed4a5b419cf
James Blair <james@blair.nz>
2026-07-09 09:14:27 +1200

    Delete redundant TicketData.tsx

--- components/TicketData.tsx
+++
@@ -1,9 +0,0 @@
-import { Ticket } from "@/lib/game/tickets";
-
-export function TicketData({ ticket }: { ticket: Ticket }) {
-  return (
-    <pre className="border p-2 whitespace-pre-wrap" suppressHydrationWarning>
-      {JSON.stringify(ticket, null, 2)}
-    </pre>
-  );
-}
