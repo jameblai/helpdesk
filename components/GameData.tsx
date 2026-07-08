97bfa1c1270a489f6f0f1d18021e8eeb888a61fb
James Blair <james@blair.nz>
2026-07-09 04:08:39 +1200

    Add whitespace-pre-wrap class to GameData

--- components/GameData.tsx
+++ components/GameData.tsx
@@ -5,7 +5,10 @@ import { useGame } from "@/lib/game";
 export function GameData() {
   const game = useGame();
   return (
-    <pre className="p-2 bg-foreground text-background" suppressHydrationWarning>
+    <pre
+      className="p-2 bg-foreground text-background whitespace-pre-wrap"
+      suppressHydrationWarning
+    >
       {JSON.stringify(game, null, 2)}
     </pre>
   );
